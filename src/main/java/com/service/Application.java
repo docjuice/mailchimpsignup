29be8dc7178ecd49395fbd01041005c7b32bb193
sharuev <sharuev@protei.ru>
2018-11-14 13:09:00 +0300

    Initial commit

---
+++ src/main/java/com/service/Application.java
@@ -0,0 +1,8 @@
+package com.service;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/service/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMainMethod() {
+
+    }
+}
